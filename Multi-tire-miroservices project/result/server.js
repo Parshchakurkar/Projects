2e0656facc17ca0a4f2fdcacc7f711deafc64167
parshchakurkar <parshchakurkar@gmail.com>
2025-10-03 14:44:35 +0530

    updated the docker and docker compose

--- Multi-tire-miroservices project/result/server.js
+++ Multi-tire-miroservices project/result/server.js
@@ -1,77 +1,86 @@
-var express = require('express'),
-    async = require('async'),
-    { Pool } = require('pg'),
-    cookieParser = require('cookie-parser'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+const express = require('express');
+const async = require('async');
+const { Pool } = require('pg');
+const cookieParser = require('cookie-parser');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
 
-var port = process.env.PORT || 4000;
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+
+const port = process.env.PORT || 4000;
 
 io.on('connection', function (socket) {
-
-  socket.emit('message', { text : 'Welcome!' });
+  socket.emit('message', { text: 'Welcome!' });
 
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
   });
 });
 
-var pool = new Pool({
+// Postgres pool
+const pool = new Pool({
   connectionString: 'postgres://postgres:postgres@db/postgres'
 });
 
+// Wait for DB
 async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
+  { times: 1000, interval: 1000 },
+  function (callback) {
+    pool.connect(function (err, client, done) {
       if (err) {
-        console.error("Waiting for db");
+        console.error("Waiting for db...");
+      } else {
+        done(); // release immediately
       }
-      callback(err, client);
+      callback(err);
     });
   },
-  function(err, client) {
+  function (err) {
     if (err) {
-      return console.error("Giving up");
+      return console.error("Giving up connecting to db");
     }
     console.log("Connected to db");
-    getVotes(client);
+    pollVotes();
   }
 );
 
-function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+// Query votes periodically
+function pollVotes() {
+  pool.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', (err, result) => {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
-      var votes = collectVotesFromResult(result);
+      const votes = collectVotesFromResult(result);
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function() {getVotes(client) }, 1000);
+    setTimeout(pollVotes, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+  const votes = { a: 0, b: 0 };
 
-  result.rows.forEach(function (row) {
-    votes[row.vote] = parseInt(row.count);
+  result.rows.forEach(row => {
+    votes[row.vote] = parseInt(row.count, 10);
   });
 
   return votes;
 }
 
+// Middlewares
 app.use(cookieParser());
-app.use(express.urlencoded());
-app.use(express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/index.html'));
 });
 
-server.listen(port, function () {
-  var port = server.address().port;
+server.listen(port, () => {
   console.log('App running on port ' + port);
 });
